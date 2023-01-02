e4b5c0b66ff8b170cbf4cb9e6b7dd5a2b8e3fa6f
Ross Warren <rosswarren4@gmail.com>
2023-01-02 13:47:58 +0000

    version 0.0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="epevermodbus",
-    version="0.0.13",
+    version="0.0.14",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
