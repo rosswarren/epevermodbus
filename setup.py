87910eb30ff9e6b217a3556cc4d63f7a884528c8
Ross Warren <rosswarren4@gmail.com>
2023-11-03 22:50:52 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="epevermodbus",
-    version="0.0.16",
+    version="0.0.17",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
